1eced6120155eeddac605e2de9b6c554fd7c1ab2
Akbar Umetov <aumetov7@gmail.com>
2020-07-13 19:26:02 +0300

    Design for NewPlaceTableViewController

---
+++ MyPlaces/NewPlaceTableViewController.swift
@@ -0,0 +1,40 @@
+//
+//  NewPlaceTableViewController.swift
+//  MyPlaces
+//
+//  Created by Акбар Уметов on 13.07.2020.
+//  Copyright © 2020 Акбар Уметов. All rights reserved.
+//
+
+import UIKit
+
+class NewPlaceTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.tableFooterView = UIView()
+    }
+    
+    // MARK: - Table view delegate
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if indexPath.row == 0 {
+            
+        } else {
+            view.endEditing(true)
+        }
+    }
+
+}
+
+// MARK: - Text field delegate
+
+extension NewPlaceTableViewController: UITextFieldDelegate {
+    // Скрываем клавиатуру по нажатию на Done
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+}
