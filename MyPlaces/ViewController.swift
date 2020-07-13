3c49014240e6cac5163f172a3cc1e8f18c9d3217
Akbar Umetov <aumetov7@gmail.com>
2020-07-13 16:48:05 +0300

    Initial Commit

---
+++ MyPlaces/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyPlaces
+//
+//  Created by Акбар Уметов on 13.07.2020.
+//  Copyright © 2020 Акбар Уметов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
