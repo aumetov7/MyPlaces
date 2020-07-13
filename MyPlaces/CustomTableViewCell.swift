522d0a57fc1fcf3f52e936863f6297194787ed71
Akbar Umetov <aumetov7@gmail.com>
2020-07-13 18:15:28 +0300

    add outlets for new labels create new class for those labels small decorative changes in main view controller (storyboard)

---
+++ MyPlaces/CustomTableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  CustomTableViewCell.swift
+//  MyPlaces
+//
+//  Created by Акбар Уметов on 13.07.2020.
+//  Copyright © 2020 Акбар Уметов. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var imageOfPlace: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var typeLabel: UILabel!
+    
+}
